52dbd9a9fa2b9a44f9c1f63b41e82e76a3cca4d2
John Adair <john@minml.net>
2013-07-01 12:59:02 -0700

    readonly ratings made completely static so that it own't choke on extremely long lists

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -21,14 +21,25 @@ module Helpers
     if !readonly && disable_after_rate
       readonly = current_user.present? ? !rateable_obj.can_rate?(current_user.id, dimension) : true
     end
-
-    content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
-                "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
-                "data-disable-after-rate" => disable_after_rate,
-                "data-readonly" => readonly,
-                "data-star-count" => star
+    
+    if readonly
+      hints = ['not rated', 'bad', 'average', 'good', 'great', 'amazing']
+      title = hints[avg]
+      content = ''
+      1.upto(star).each do |idx|
+        img = idx <= avg ? 'star-on.png' : 'star-off.png'
+        content += image_tag(img, title:title, alt: idx)
+        content += '&nbsp;'
+      end
+      content_tag :div, content.html_safe
+    else
+      content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
+                            "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
+                            "data-disable-after-rate" => disable_after_rate,
+                            "data-readonly" => readonly,
+                            "data-star-count" => star
+    end
   end
-
 end
 
 class ActionView::Base
